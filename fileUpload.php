e2eb782926408772232fc736f01f211b7c1ec669
Christoph Brandstetter <christoph.br4ndstetter@gmail.com>
2023-02-27 22:04:53 +0100

    File upload

--- fileUpload.php
+++ fileUpload.php
@@ -1,6 +1,29 @@
 <?php
 require("includes/common.inc.php"); //inlcudes my funktion to print/check Arrays
 ta($_FILES); 
+
+
+$uploaddir = 'includes/';
+$uploadfile = $uploaddir . basename($_FILES['UL']['name']);
+
+if(count($_FILES) > 0){
+
+  $upl = $_FILES["UL"];
+
+  if($upl["error"] == 0){
+    $ok = move_uploaded_file($upl["tmp_name"], $uploadfile);
+    if($ok) {
+			$msg = '<p class="success">Die Datei wurde erfolgreich hochgeladen.</p>';
+		}
+		else {
+			$msg = '<p class="error">Leider konnte die Datei nicht am Zielort gespeichert werden.</p>';
+		}
+	}
+	else {
+		$msg = '<p class="error">Während des Uploads ist (irgendein) Fehler aufgetreten.</p>';
+	}
+}
+
 ?>
 
 <!doctype html>
@@ -8,12 +31,13 @@ ta($_FILES);
 <head>
   <meta charset="utf-8">
   <title>File Upload</title>
-  <link rel="stylesheet" href="css/common.css">
+  <link rel="stylesheet" href="css/common.css"> <!--Includes styling fpor output arrays -->
 </head>
 <body>
+  <?php echo($msg);?>
   <form method="post" enctype="multipart/form-data">
-    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="file" name="UL" id="fileToUpload">
     <input type="submit" value="Upload" name="submit">
   </form>
 </body>
-</html>
+</html>    
